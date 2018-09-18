3c048b73630a8b5735ac4f8edf22830ec27d43aa
kouta <h27i47@ed.cc.suzuka-ct.ac.jp>
2018-09-19 00:50:15 +0900

    グローバル変数を追加

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include<algorithm>
 using namespace std;
 
+int width,height;//盤面の横幅と縦幅
+int table_score[100][100]//各マスのスコア
+int table_state[100][100]//各マスの状況(どっちのチームのマスか、エージェントは存在するかetc..)
 
 int main(){
 
